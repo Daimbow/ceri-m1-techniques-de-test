8236376c84230aae16500967445bf6766901eb21
Daimbow <julien-jamme@outlook.com>
2024-11-15 09:54:00 +0100

    test tp4 classe PokemonTrainerFactory

--- src/main/java/fr/univavignon/pokedex/api/PokemonMetadataProvider.java
+++ src/main/java/fr/univavignon/pokedex/api/PokemonMetadataProvider.java
@@ -4,16 +4,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 
+package fr.univavignon.pokedex.api;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class PokemonMetadataProvider implements IPokemonMetadataProvider {
 
     private final Map<Integer, PokemonMetadata> pokemonMetadataMap;
 
-    
     public PokemonMetadataProvider() {
         this.pokemonMetadataMap = new HashMap<>();
+        pokemonMetadataMap.put(0, new PokemonMetadata(0, "Bulbizarre", 126, 126, 90));
+        pokemonMetadataMap.put(133, new PokemonMetadata(133, "Aquali", 186, 168, 260));
+        pokemonMetadataMap.put(150, new PokemonMetadata(150, "Mew", 186, 168, 260));
     }
 
-    
     @Override
     public PokemonMetadata getPokemonMetadata(int index) throws PokedexException {
         if (!pokemonMetadataMap.containsKey(index)) {
@@ -22,3 +28,4 @@ public class PokemonMetadataProvider implements IPokemonMetadataProvider {
         return pokemonMetadataMap.get(index);
     }
 }
+
