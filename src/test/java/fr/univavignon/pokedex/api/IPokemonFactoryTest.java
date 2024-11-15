a3f624a70e00c391cbc464390f965005e930f265
Daimbow <julien-jamme@outlook.com>
2024-11-15 10:30:47 +0100

    test tp4 classe PokemonFactoryTest

--- src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -288,19 +288,7 @@ public class IPokemonFactoryTest {
         assertEquals(90, pokemon.getStamina());  
     }
 
-    @Test
-    public void testCreatePokemon_ValidValues_MaxStat_Imp() {
-        IPokemonMetadataProvider metadataProvider = new PokemonMetadataProvider();
-        IPokemonFactory pokemonFactory = new PokemonFactory(metadataProvider);
-        
-        Pokemon pokemon = pokemonFactory.createPokemon(0, 613, 64, 4000, 4);
 
-        assertNotNull(pokemon);
-
-        assertEquals(126, pokemon.getAttack());   
-        assertEquals(126, pokemon.getDefense());  
-        assertEquals(90, pokemon.getStamina());
-    }
 
 
 
