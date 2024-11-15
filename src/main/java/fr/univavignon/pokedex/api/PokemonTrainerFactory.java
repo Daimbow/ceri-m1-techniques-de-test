0738a5388f9ef237a5d6fff2ca65e251988fe523
Daimbow <julien-jamme@outlook.com>
2024-11-15 09:40:09 +0100

    test tp4 classe PokemonTrainerFactory

--- src/main/java/fr/univavignon/pokedex/api/PokemonTrainerFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/PokemonTrainerFactory.java
@@ -4,12 +4,16 @@ package fr.univavignon.pokedex.api;
 public class PokemonTrainerFactory implements IPokemonTrainerFactory {
 
 
-    @Override
-    public PokemonTrainer createTrainer(String name, Team team, IPokedexFactory pokedexFactory) {
-
-    	IPokedex pokedex = pokedexFactory.createPokedex(new PokemonMetadataProvider(), new PokemonFactory(new PokemonMetadataProvider()));
-        
-
-    	return new PokemonTrainer(name, team, pokedex);
-    }
+	   @Override
+	    public PokemonTrainer createTrainer(String name, Team team, IPokedexFactory pokedexFactory) {
+	        // Validate inputs
+	        if (name == null || name.isEmpty() || team == null || pokedexFactory == null) {
+	            return null;
+	        }
+	        
+	        IPokedex pokedex = pokedexFactory.createPokedex(new PokemonMetadataProvider(), new PokemonFactory(new PokemonMetadataProvider()));;
+	        
+	        // Return the new PokemonTrainer instance
+	        return new PokemonTrainer(name, team, pokedex);
+	    }
 }
