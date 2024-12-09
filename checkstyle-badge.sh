00e658cd3e34d5ff9db1ed7e1316dd72995cb295
Daimbow <julien-jamme@outlook.com>
2024-12-09 19:50:17 +0100

    Test badge détailler pour checkstyle

--- checkstyle-badge.sh
+++ checkstyle-badge.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ ! -f target/checkstyle/checkstyle-result.xml ]; then
+if [ ! -f target/checkstyle-result.xml ]; then
     echo "Le fichier Checkstyle XML n'existe pas."
     exit 1
 fi
 
 # On Compte les erreurs et avertissements dans le rapport Checkstyle
-errors=$(xmllint --xpath "count(//error[@severity='error'])" target/checkstyle/checkstyle-result.xml)
-warnings=$(xmllint --xpath "count(//error[@severity='warning'])" target/checkstyle/checkstyle-result.xml)
+errors=$(xmllint --xpath "count(//error[@severity='error'])" target/checkstyle-result.xml)
+warnings=$(xmllint --xpath "count(//error[@severity='warning'])" target/checkstyle-result.xml)
 
 if [ "$errors" -gt 0 ]; then
     badge="Errors:$errors-red"  
@@ -21,4 +21,4 @@ badge_url="https://img.shields.io/badge/$badge?style=flat-square"
 
 echo "Badge URL : $badge_url"
 
-echo $badge_url > target/checkstyle/checkstyle-badge-url.txt
+echo $badge_url > target/checkstyle-badge-url.txt
